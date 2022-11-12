0d44d918ad97148d4bff02965412693f7952cde5
Piotr Cenda <piotr.cenda222@gmail.com>
2022-11-12 13:16:35 +0100

    NEW BEGINNING

--- README.md
+++
@@ -1,1 +0,0 @@
-# books-to-color-palette
--- book2palette.py
+++ book2palette.py
@@ -1,16 +1,20 @@
 import os
-import text2emotion as te
+from transformers import pipeline
 
+emotions = ["love", "admiration", "joy", "pproval", "caring", "excitement", "amusement", "gratitude", "desire", "anger", "optimism", "disapproval", "grief", "annoyance", "pride", "curiosity", "neutral", "disgust", "disappointment", "realization", "fear", "relief", "confusion", "remorse", "embarrassment", "surprise", "sadness, nervousness]
 
 def load_all_texts(texts_folder_path: str = "./data/texts"):
     txt_paths = [os.path.join(texts_folder_path, file) for file in os.listdir(texts_folder_path)]
+    text = []
     
     for path in txt_paths:
         with open(path, "r") as file:
-            text = file.readlines()
-            print(type(text))
-        break
+            text.append(file.readlines()[0])
+
+    return text
 
 
 if __name__ == "__main__":
-    load_all_texts()
+    emotion = pipeline('sentiment-analysis', model='arpanghoshal/EmoRoBERTa')
+    emotion_labels = emotion("I hate mondays, pls kill me")
+    print(emotion_labels)
