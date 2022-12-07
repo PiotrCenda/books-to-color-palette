85edf4ba2a69249dc4095dcf3e423626aaf21cf7
acieslak333 <adam.cieslak333@gmail.com>
2022-12-07 21:09:31 +0100

    3d visualizer update

--- 3d_visualiser.py
+++ 3d_visualiser.py
@@ -29,7 +29,7 @@ def main():
     np.random.seed(seed=42)
     
     thr1 = 0.75
-    thr2 = 0.25
+    thr2 = 0.35
     
     in_shape = (32, 256, 256)
     pic1 = np.zeros(in_shape)
@@ -43,13 +43,14 @@ def main():
 
     # pic1 = normalize(pic)
     # pic2 = normalize(pic2)
-
-
+    new_pic1 = []
+    new_pic2 = []
+    new_pic = []
     for i, (slice1, slice2) in tqdm(enumerate(zip(pic1, pic2))):
         # print(i)
         in_shape_slice = (1, 256, 256) 
         rotate_factor = 1/16
-        distance = 64
+        distance = 96
         x1 = np.cos(np.pi + i*np.pi*rotate_factor) * distance
         x2 = np.cos(i*np.pi*rotate_factor) * distance
         y1 = np.sin(np.pi + i*np.pi*rotate_factor) * distance
@@ -57,27 +58,44 @@ def main():
 
         distance_map = distance_from_center(in_shape_slice, np.array([[128 + x1, 128 + y1]]))
         distance_map2 = distance_from_center(in_shape_slice, np.array([[128 + x2, 128 + y2]]))
+        
+        s = np.random.uniform(0,2,slice1.shape)
+        # s = np.stack((s,)*3, axis=-1)
+        # plt.figure()
+        # plt.imshow(s)
+        # plt.show()
 
         slice1 -= distance_map[0]
         slice2 -= distance_map2[0]
         slice1 = normalize(slice1)
         slice2 = normalize(slice2)
-        pic1[i] = slice1
-        pic2[i] = slice2
+        slice1 = normalize(np.array(slice1 > thr1).astype(np.int8) * (1- normalize((distance_map * s ))))
+        slice2 = normalize(np.array(slice2 > thr2).astype(np.int8) * (1 - normalize((distance_map2 * s )))) 
+        slice1 = normalize(np.array(slice1 > 0.5).astype(np.int8))
+        slice2 = normalize(np.array(slice2 > 0.5).astype(np.int8))
+
+        slice1 = np.stack((slice1,)*3, axis=-1) * (209, 239, 44) 
+        slice2 = np.stack((slice2,)*3, axis=-1) * (132, 236, 207) 
+
+        new_pic1.append(slice1) 
+        new_pic2.append(slice2) 
  
 
-    
-    pic1 = np.array(pic1 > thr1).astype(np.int8)
-    pic2 = np.array(pic2 > thr2).astype(np.int8)*2
+    new_pic1 = np.array(new_pic1)
+    new_pic2 = np.array(new_pic2)
+    # pic1 = np.array(pic1 > thr1).astype(np.int8)
+    # pic2 = np.array(pic2 > thr2).astype(np.int8)*2
     
     # print(np.unique)
-    pic = pic1 + pic2
-    pic[pic==3] = 1
+    pic = new_pic1 + new_pic2
+    # pic[pic==3] = 1
 
     print(np.unique(pic))
     for n in range(32):
+        print(pic[n][0].shape)
+        print(np.unique(pic[n][0]))
         plt.figure()
-        plt.imshow(pic[n])
+        plt.imshow(normalize(pic[n][0]))
         plt.show()
     
 if __name__ == "__main__":
